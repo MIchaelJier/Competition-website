cf1a928ac32710183f18b5c1fb70749cd6a1ec12
Ran Lin <moxdopv754@163.com>
2019-06-04 16:34:44 +0800

    Initial commit

---
+++ src/main/java/com/team5101/xkjs/XkjsApplication.java
@@ -0,0 +1,13 @@
+package com.team5101.xkjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XkjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XkjsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team5101/xkjs/XkjsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.team5101.xkjs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XkjsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
