d63be897ff458f6f112827b1d0462ec113a27e78
ggl162 <guogangling18@163.com>
2019-06-30 15:58:48 +0800

    fix bug

--- src/main/java/com/team5101/mapper/SignUpMapper.java
+++ src/main/java/com/team5101/mapper/SignUpMapper.java
@@ -11,16 +11,7 @@ import java.util.List;
 @Repository
 public interface SignUpMapper {
 
-//    查询列表接口
-//    @Select("SELECT * FROM x_contesttp")
-//    @Results({
-//        @Result(property = "u_id", column = "c_id",
-//                one = @One(select = "com.team5101.mapper.CompetitorMapper.getCompetitorId")),
-//        @Result(property = "j_id", column = "j_id",
-//                many = @Many(select = "com.team5101.mapper.ContestInfoMapper.getContestId")),
-//
-//        //column代表将该属性作为参数传入,property代表将查询出来的数据交给该属性
-//})
+     // 查询列表接口
     List<SignUp> getAllContestByJno(Integer j_id);
     //插入报名信息
     Integer addOne(SignUp signUp);
